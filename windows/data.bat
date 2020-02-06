dd2aa956f69ea717a12ee2123561ec372e5ea3f3
Gatis Jansons <gatis.jansons@squalio.com>
2020-02-06 17:48:19 +0200

    cleanup

--- windows/data.bat
+++
@@ -1,5 +0,0 @@
-bitsadmin /Transfer x  http://www.atd.lv/sites/default/files/GTFS/gtfs-latvia-lv.zip c:\temp\gotten.zip
-powershell Expand-Archive -Force c:\temp\gotten.zip C:\temp\result
-cd C:\temp\result
-type stops.txt | find "Gauja"
-
