b0415ba2826cc75c0cd01543886c64781b3e4e2a
Ubuntu <student@stud07.ylz3gnbuoigevdvttpb1eebepb.ax.internal.cloudapp.net>
2020-02-18 04:31:55 +0000

    le

--- test/prog.py
+++ test/prog.py
@@ -1,37 +1,15 @@
-def printMaxActivities(sf):
-    n = len(sf)
-
-    # The first activity is always selected
-    i = 0
-
-    count = 1
-
-    # Consider rest of the activities
-    for j in range(1, n):
-
-        # If this activity has start time greater than
-        # or equal to the finish time of previously
-        # selected activity, then select it
-        if sf[j][0] > sf[i][1]:
-            i = j
-            count += 1
-
-    print(f'{count}')
-
-
-N = int(input())
-# start = []
-# finish = []
-fs = []
+N, K = map(int, input().split())
+counts = dict()
 for n in range(N):
-    datestring1, datestring2 = input().split()
-    d1, m1, y1 = datestring1.split('.')
-    d2, m2, y2 = datestring2.split('.')
-    date1 = y1 + m1 + d1
-    date2 = y2 + m2 + d2
-    pair = date1, date2
-    fs.append(pair)
+    x = int(input())
+    if x in counts:
+        counts[x] += 1
+    else:
+        counts[x] = 1
 
-fs.sort(key=lambda x: x[1])
+for (key, val) in counts.items():
+    if val == 1:
+        print(key)
+        break
 
-printMaxActivities(fs)
+
---
+++ test/test.sh
@@ -0,0 +1,12 @@
+python3 prog.py < data/VIRKNE.I0 > temp.out
+diff data/VIRKNE.O0 temp.out
+
+python3 prog.py < data/VIRKNE.I1 > temp.out
+diff data/VIRKNE.O1 temp.out
+
+python3 prog.py < data/VIRKNE.I2 > temp.out
+diff data/VIRKNE.O2 temp.out
+
+python3 prog.py < data/VIRKNE.I3 > temp.out
+diff data/VIRKNE.O3 temp.out
+
